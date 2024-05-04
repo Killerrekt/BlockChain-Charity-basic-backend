aa7f521160eddc32cc66244d18c21fcb719b5447
Killerrekt <amankhanter14@gmail.com>
2024-05-04 21:37:05 +0530

    cors

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const data = require("./model.js");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -14,6 +15,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/add-org", async (req, res) => {
   try {
